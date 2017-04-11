ce04bb411064e5b430206364533e9ba04d6937de
Patrick J Jimison <jimisonp@athena.ecs.csus.edu>
2017-04-10 17:29:16 -0700

    changed filenames for angular and php scripts

---
+++ get_students.php
@@ -0,0 +1,15 @@
+<?php
+//database settings
+$config = parse_ini_file('./config.ini');
+
+$connect = mysqli_connect("athena.ecs.csus.edu", $config['username'], $config['password'], $config['dbname']);
+
+$result = mysqli_query($connect, "SELECT * FROM student");
+
+$data = array();
+
+while ($row = mysqli_fetch_array($result)) {
+  $data[] = $row;
+}
+    print json_encode($data);
+?>
